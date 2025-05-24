1a8fb502ada01464abae670358ccf5b35adff5d3
pushkarpoman <pushkarpoman10@gmail.com>
2025-05-24 15:25:36 +0530

    Add files via upload

---
+++ program371.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+class DoublyCL
+{
+    public:
+        PNODE head;
+        PNODE tail;
+        int iCount;
+
+        DoublyCL()
+        {
+            head = NULL;
+            tail = NULL;
+            iCount = 0;
+        }
+
+        void InsertFirst(int no){}
+        void InsertLast(int no){}
+        void InsertAtPos(int no, int ipos){}
+        
+        void DeleteFirst(){}
+        void DeleteLast(){}
+        void DeleteAtPos(int ipos){}
+
+        void Display(){}
+        int Count()
+        {
+            return iCount;
+        }
+};
+
+int main()
+{
+    DoublyCL obj;
+    int iRet = 0;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+    
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+    obj.InsertLast(121);
+    
+    obj.InsertAtPos(75,4);
+
+    obj.Display();
+    iRet = obj.Count();
+
+    cout<<"Number of elements are : "<<iRet<<"\n";
+
+    obj.DeleteAtPos(4);
+
+    obj.Display();
+    iRet = obj.Count();
+
+    cout<<"Number of elements are : "<<iRet<<"\n";
+
+    obj.DeleteFirst();
+    obj.DeleteLast();
+    
+    obj.Display();
+    iRet = obj.Count();
+
+    cout<<"Number of elements are : "<<iRet<<"\n";
+    
+    return 0;
+}
