a17a133591ad7523ec1dea6477a0c2a6d2e3567f
pushkarpoman <pushkarpoman10@gmail.com>
2025-08-05 17:27:32 +0530

    Add files via upload

---
+++ program445.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class program445
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        File fobj = new File("Demo.txt");
+
+        fobj.createNewFile();
+    }
+}
