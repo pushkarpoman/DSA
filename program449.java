4e7e17d81285a4a1e13aa5e6e845b8b20f4b2609
pushkarpoman <pushkarpoman10@gmail.com>
2025-08-09 23:02:15 +0530

    Add files via upload

---
+++ program449.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class program449
+{
+    public static void main(String Arg[])
+    {
+        try
+        {
+            boolean bret = false;
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the file name that you want to create : ");
+            String name = sobj.nextLine();
+
+            File fobj = new File(name);
+
+            if(fobj.exists())
+            {
+                System.out.println("File is already existing in the folder");
+            }
+            else
+            {
+                bret = fobj.createNewFile();
+
+                if(bret == true)
+                {
+                    System.out.println("File succesfully created");
+                }
+                else
+                {
+                    System.out.println("Unable to create the file");
+                }
+            }
+        }
+        catch(Exception eobj)
+        {
+            System.out.println("Exception occured.."+eobj);
+        }
+
+    }
+}
