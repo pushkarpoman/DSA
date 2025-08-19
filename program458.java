ba66eb1c22da46586b89d68320043b1cf98860f5
pushkarpoman <pushkarpoman10@gmail.com>
2025-08-19 08:14:07 +0530

    Add files via upload

---
+++ program458.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class program458
+{
+    public static void main(String Arg[])
+    {
+        try
+        {
+            boolean bret = false;
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the file name that you want to open : ");
+            String name = sobj.nextLine();
+
+            File fobj = new File(name);
+
+            if(fobj.exists())
+            {
+                int iRet = 0;
+
+                FileInputStream fiobj = new FileInputStream(fobj);
+
+                byte arr[] = new byte[20];
+
+                iRet = fiobj.read(arr);
+                String str = new String(arr);
+                System.out.println("Data from file is : "+str);
+                System.out.println("Return value of read is : "+iRet);
+            }
+            else
+            {
+                System.out.println("There is no such file");
+            }
+        }
+        catch(Exception eobj)
+        {
+            System.out.println("Exception occured.."+eobj);
+        }
+
+    }
+}
