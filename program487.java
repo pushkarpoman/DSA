e4bb0ff36244f01c18c4574469da4cb5197f5186
pushkarpoman <pushkarpoman10@gmail.com>
2025-09-19 18:30:27 +0530

    Add files via upload

---
+++ program487.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class program487
+{
+    public static void main(String Arg[])
+    {
+        String Header = "First.txt 10                                                                                        ";
+
+        System.out.println("Header length is : "+Header.length());
+
+        Header = Header.trim();
+
+        System.out.println("Header length after trim is : "+Header.length());
+
+        String Arr[] = Header.split(" ");
+
+        System.out.println("Number of tokens : "+Arr.length);
+    }
+}
