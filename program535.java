74b1db158ddf7b2479215e307920dfead974fc51
pushkarpoman <pushkarpoman10@gmail.com>
2025-11-21 13:57:30 +0530

    Add files via upload

---
+++ program535.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class program535
+{
+    public static void main(String Arg[])
+    {
+        LinkedList <Integer>lobj = new LinkedList<Integer>();
+
+        lobj.add(11);
+        lobj.add(21);
+        lobj.add(51);
+        
+        System.out.println(lobj);
+    }
+}
