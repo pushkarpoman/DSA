6db59ee61fca3099b9de54c43af8d9658d6c7e4f
pushkarpoman <pushkarpoman10@gmail.com>
2025-12-07 09:02:02 +0530

    Add files via upload

---
+++ program542.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class program542
+{
+    public static void main(String Arg[])
+    {
+        LinkedList <Integer>lobj = new LinkedList<Integer>();
+
+        lobj.addFirst(51);
+        lobj.addFirst(21);
+        lobj.addFirst(11);
+        
+        lobj.addLast(101);
+        lobj.addLast(111);
+        lobj.addLast(121);
+
+        lobj.add(3,75);
+        
+        for(Integer no : lobj)
+        {
+            System.out.println(no);
+        }
+
+        lobj.removeFirst();
+        lobj.removeLast();
+
+        for(Integer no : lobj)
+        {
+            System.out.println(no);
+        }
+
+        lobj.clear();
+    }
+}
