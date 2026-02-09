00f9b3d2c479c93c9a138f83bb085d5d7a5b0154
pushkarpoman <pushkarpoman10@gmail.com>
2026-02-09 10:42:02 +0530

    Add files via upload

---
+++ program589.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    char Arr[30];
+
+    cout<<"Enter your name : \n";
+    cin.getline(Arr,30);
+
+    cout<<"Your name is : "<<Arr<<"\n";
+}
